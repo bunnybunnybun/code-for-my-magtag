7fcf07eec9866551a06f1ee0a24748339399e2e1
Carlisle Poe <carlislepoe@gmail.com>
2026-06-02 15:19:18 -0700

    Added very basic weather functionality. Also added .wakatime-project file so that Hackatime time tracking actually works correctly.

--- code.py
+++ code.py
@@ -1,5 +1,9 @@
 import asyncio
 import time as og_time
+import adafruit_requests
+import os
+import wifi
+import adafruit_connection_manager
 
 from adafruit_datetime import date, datetime, time
 from adafruit_magtag.magtag import MagTag
@@ -8,6 +12,10 @@ magtag = MagTag()
 nametag_or_clock = False
 day = 0
 
+# Stuff that's too complex for my brain but is needed apparently:
+pool = adafruit_connection_manager.get_radio_socketpool(wifi.radio)
+ssl_context = adafruit_connection_manager.get_radio_ssl_context(wifi.radio)
+requests = adafruit_requests.Session(pool, ssl_context)
 
 async def clock():
     global day
@@ -22,29 +30,6 @@ async def clock():
             magtag.set_text(f"{now.hour}:{now.minute}", 1)
         await asyncio.sleep(300)
         day = now.day
-
-
-def nametag():
-    magtag.remove_all_text()
-    magtag.add_text(
-        text_position=(
-            5,
-            20,
-        ),
-        text_scale=2,
-    )
-
-    magtag.add_text(
-        text_position=(
-            5,
-            72,
-        ),
-        text_scale=6,
-    )
-
-    magtag.set_text("Hello, my name is:", 0)
-    magtag.set_text("Carlisle", 1)
-
 
 async def start_clock():
     magtag.remove_all_text()
@@ -73,6 +58,56 @@ async def start_clock():
     clock_task = asyncio.create_task(clock())
     await asyncio.gather(clock_task)
 
+def nametag():
+    magtag.remove_all_text()
+    magtag.add_text(
+        text_position=(
+            5,
+            20,
+        ),
+        text_scale=2,
+    )
+
+    magtag.add_text(
+        text_position=(
+            5,
+            72,
+        ),
+        text_scale=6,
+    )
+
+    magtag.set_text("Hello, my name is:", 0)
+    magtag.set_text("Carlisle", 1)
+
+async def weather():
+    ssid = os.getenv("CIRCUITPY_WIFI_SSID")
+    password = os.getenv("CIRCUITPY_WIFI_PASSWORD")
+    try:
+        # Connect to the Wi-Fi network
+        wifi.radio.connect(ssid, password)
+    except OSError as e:
+        print(f"❌ OSError: {e}")
+
+    magtag.network.connect()
+    weather_url = "https://api.open-meteo.com/v1/forecast?latitude=45.8207&longitude=-120.8217&temperature_unit=fahrenheit&timezone=auto&current=temperature,apparent_temperature"
+
+    weather_response = requests.get(weather_url).json()
+    try:
+        temperature_unit = weather_response["current_units"]["temperature"]
+        current_temp = weather_response["current"]["temperature"]
+        apparent_temp = weather_response["current"]["apparent_temperature"]
+    except (KeyError, TypeError):
+        print("Something went wrong :(")
+    print(f"Response: {weather_response}")
+    magtag.remove_all_text()
+    magtag.add_text(
+        text_position=(
+            30,
+            60,
+        ),
+        text_scale=7,
+    )
+    magtag.set_text(f"{current_temp}{temperature_unit}", 0)
 
 magtag.peripherals.neopixel_disable = True
 
@@ -106,7 +141,7 @@ async def listen_for_button_presses():
 
 
 async def main():
-    start_clock_task = asyncio.create_task(start_clock())
+    start_clock_task = asyncio.create_task(weather())
     listen_for_button_presses_task = asyncio.create_task(listen_for_button_presses())
     await asyncio.gather(start_clock_task, listen_for_button_presses_task)
 
