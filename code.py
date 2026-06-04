bc8422746921471c2ffc9ab9445f71becaaab402
Carlisle Poe <carlislepoe@gmail.com>
2026-06-04 09:52:04 -0700

    Added min and max temp to weather section. Restructured code a bit to make it like less buggy :D

--- code.py
+++ code.py
@@ -9,7 +9,6 @@ from adafruit_datetime import date, datetime, time
 from adafruit_magtag.magtag import MagTag
 
 magtag = MagTag()
-nametag_or_clock = False
 day = 0
 
 # Stuff that's too complex for my brain but is needed apparently:
@@ -19,17 +18,16 @@ requests = adafruit_requests.Session(pool, ssl_context)
 
 async def clock():
     global day
-    while not nametag_or_clock:
-        now = datetime.now()
-        print(now)
-        if day != now.day:
-            magtag.set_text(f"{now.year}-{now.month}-{now.day}", 0)
-        if now.minute < 10:
-            magtag.set_text(f"{now.hour}:0{now.minute}", 1)
-        else:
-            magtag.set_text(f"{now.hour}:{now.minute}", 1)
-        await asyncio.sleep(300)
-        day = now.day
+    now = datetime.now()
+    print(now)
+    if day != now.day:
+        magtag.set_text(f"{now.year}-{now.month}-{now.day}", 0)
+    if now.minute < 10:
+        magtag.set_text(f"{now.hour}:0{now.minute}", 1)
+    else:
+        magtag.set_text(f"{now.hour}:{now.minute}", 1)
+    await asyncio.sleep(300)
+    day = now.day
 
 async def start_clock():
     magtag.remove_all_text()
@@ -55,10 +53,7 @@ async def start_clock():
     except Exception as error:
         print(error)
 
-    clock_task = asyncio.create_task(clock())
-    await asyncio.gather(clock_task)
-
-def nametag():
+async def nametag():
     magtag.remove_all_text()
     magtag.add_text(
         text_position=(
@@ -83,37 +78,68 @@ async def weather():
     ssid = os.getenv("CIRCUITPY_WIFI_SSID")
     password = os.getenv("CIRCUITPY_WIFI_PASSWORD")
     try:
-        # Connect to the Wi-Fi network
         wifi.radio.connect(ssid, password)
     except OSError as e:
+        magtag.add_text(
+            text_position=(
+                10,
+                30,
+            ),
+            text_scale=1,
+        )
+        magtag.set_text(f"Could not connect to wifi.\nError: {e}", 0)
         print(f"❌ OSError: {e}")
 
     magtag.network.connect()
-    weather_url = "https://api.open-meteo.com/v1/forecast?latitude=45.8207&longitude=-120.8217&temperature_unit=fahrenheit&timezone=auto&current=temperature,apparent_temperature"
+    weather_url = "https://api.open-meteo.com/v1/forecast?latitude=45.8207&longitude=-120.8217&daily=temperature_2m_max,temperature_2m_min,cloud_cover_mean&current=temperature_2m,apparent_temperature&timezone=auto&forecast_days=3&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch"
 
     weather_response = requests.get(weather_url).json()
     try:
-        temperature_unit = weather_response["current_units"]["temperature"]
-        current_temp = weather_response["current"]["temperature"]
-        apparent_temp = weather_response["current"]["apparent_temperature"]
+        temperature_unit = weather_response["current_units"]["temperature_2m"]
+        current_temp = weather_response["current"]["temperature_2m"]
+        current_apparent_temp = weather_response["current"]["apparent_temperature"]
+        today_max_temp = weather_response["daily"]["temperature_2m_max"][0]
+        today_min_temp = weather_response["daily"]["temperature_2m_min"][0]
     except (KeyError, TypeError):
         print("Something went wrong :(")
     print(f"Response: {weather_response}")
     magtag.remove_all_text()
     magtag.add_text(
         text_position=(
-            30,
-            60,
+            15,
+            35,
         ),
-        text_scale=7,
+        text_scale=5,
+    )
+    magtag.add_text(
+        text_position=(
+            10,
+            70,
+        ),
+        text_scale=2,
+    )
+    magtag.add_text(
+        text_position=(
+            210,
+            20,
+        ),
+        text_scale=2,
+    )
+    magtag.add_text(
+        text_position=(
+            210,
+            50,
+        ),
+        text_scale=2,
     )
     magtag.set_text(f"{current_temp}{temperature_unit}", 0)
+    magtag.set_text(f"Feels like {current_apparent_temp}{temperature_unit}", 1)
+    magtag.set_text(f"Max: {today_max_temp}",2)
+    magtag.set_text(f"Min: {today_min_temp}",3)
 
 magtag.peripherals.neopixel_disable = True
 
-
 async def listen_for_button_presses():
-    global nametag_or_clock
     while True:
         if not magtag.peripherals.buttons[0].value:
             print("Button a pressed")
@@ -125,25 +151,41 @@ async def listen_for_button_presses():
                 magtag.peripherals.neopixels.fill((240, 0, 255))
                 await asyncio.sleep(0.5)
 
+        elif not magtag.peripherals.buttons[1].value:
+            print("Button b pressed")
+            weather_task = asyncio.create_task(weather())
+            try:
+                clock_task.cancel()
+                print("Cancelled clock_task")
+            except Exception as exception:
+                print(f"Could not cancel clock_task because: {exception}")
+
         elif not magtag.peripherals.buttons[2].value:
             print("Button c pressed")
-            if nametag_or_clock:
-                nametag_or_clock = not nametag_or_clock
-                asyncio.run(start_clock())
-            else:
-                nametag_or_clock = not nametag_or_clock
-                nametag()
+            await start_clock()
+            try:
+                clock_task.cancel()
+                print("Cancelled clock_task")
+            except Exception as exception:
+                print(f"Could not cancel clock_task because: {exception}")
+            clock_task = asyncio.create_task(clock())
+
         elif not magtag.peripherals.buttons[3].value:
             print("Button d pressed")
-            magtag.refresh()
+            nametag_task = asyncio.create_task(nametag())
+            try:
+                clock_task.cancel()
+                print("Cancelled clock_task")
+            except Exception as exception:
+                print(f"Could not cancel clock_task because: {exception}")
 
         await asyncio.sleep(0.05)
 
 
 async def main():
-    start_clock_task = asyncio.create_task(weather())
+    weather_task = asyncio.create_task(weather())
     listen_for_button_presses_task = asyncio.create_task(listen_for_button_presses())
-    await asyncio.gather(start_clock_task, listen_for_button_presses_task)
+    await asyncio.gather(weather_task, listen_for_button_presses_task)
 
 
 asyncio.run(main())
